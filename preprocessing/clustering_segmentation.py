8c84cfc2999c2cab11d59806fb7e5661d365c62b
ahmedehabb <ahmedehabb17@gmail.com>
2023-05-10 00:04:09 +0200

    added clustering and region segmentation

---
+++ preprocessing/clustering_segmentation.py
@@ -0,0 +1,61 @@
+from skimage.color import rgb2gray
+from skimage.filters import threshold_otsu
+from skimage.segmentation import slic
+from skimage.segmentation import mark_boundaries
+from skimage.util import img_as_float
+from skimage.transform import resize
+from skimage.measure import regionprops
+from skimage.color import label2rgb
+from sklearn.cluster import KMeans
+from skimage import io
+import numpy as np
+
+class ClusteringSegmentation:
+    def __init__(self, method='kmeans', n_clusters=3, compactness=30.0, sigma=1.0):
+        self.method = method
+        self.n_clusters = n_clusters
+        self.compactness = compactness
+        self.sigma = sigma
+
+    def process(self, image):
+        # convert the image to grayscale
+        gray_image = rgb2gray(image)
+
+        # apply SLIC algorithm to get superpixels
+        scaled_image = img_as_float(resize(image, (500, 500)))
+        segments = slic(scaled_image, n_segments=300, compactness=self.compactness, sigma=self.sigma)
+
+        # calculate the color features of each superpixel
+        features = []
+        for i in np.unique(segments):
+            mask = segments == i
+            feature = np.mean(gray_image[mask])
+            features.append(feature)
+        features = np.array(features)
+
+        # cluster the superpixels based on their color features
+        if self.method == 'kmeans':
+            kmeans = KMeans(n_clusters=self.n_clusters).fit(features.reshape(-1, 1))
+            labels = kmeans.labels_
+        elif self.method == 'fcm':
+            from skimage.segmentation import fuzzy_cmeans
+            centers, labels = fuzzy_cmeans(features.reshape(-1, 1), self.n_clusters, m=2, error=0.005, maxiter=100, init=None)
+
+        # create an image with each superpixel labeled by its cluster
+        labels = np.array(labels, dtype=np.float)
+        label_image = np.zeros_like(segments)
+        for i, label in enumerate(np.unique(segments)):
+            mask = segments == label
+            label_image[mask] = labels[i]
+
+        # post-process the labeled image
+        label_image = label_image.astype(int)
+        regions = regionprops(label_image)
+        for i, region in enumerate(regions):
+            if region.area < 500:
+                label_image[label_image == i+1] = 0
+
+        # color the regions for visualization
+        labeled_regions = label2rgb(label_image, image=image, bg_label=0, kind='avg')
+
+        return labeled_regions
---
+++ preprocessing/region_segmentation.py
@@ -0,0 +1,43 @@
+import numpy as np
+from skimage.segmentation import felzenszwalb, slic, quickshift
+from skimage.color import rgb2gray
+from skimage.filters import sobel
+from scipy import ndimage as ndi
+
+class RegionBasedSegmentation:
+    def __init__(self, method="region_growing", **kwargs):
+        self.method = method
+        self.kwargs = kwargs
+    
+    def process(self, image):
+        # Convert the image to grayscale
+        image = rgb2gray(image)
+        
+        if self.method == "region_growing":
+            # Region Growing segmentation
+            threshold = self.kwargs.get('threshold', 0.5)
+            seed_point = self.kwargs.get('seed_point', None)
+            if seed_point is None:
+                seed_point = (image.shape[0]//2, image.shape[1]//2)
+            mask = np.zeros_like(image)
+            mask[seed_point] = 1
+            segmented = ndi.binary_fill_holes(ndi.binary_dilation(mask, iterations=2))
+            return segmented
+        elif self.method == "region_splitting":
+            # Region Splitting segmentation
+            min_size = self.kwargs.get('min_size', 50)
+            max_size = self.kwargs.get('max_size', 1000)
+            segmented = felzenszwalb(image, scale=100, sigma=0.5, min_size=min_size, max_size=max_size)
+            return segmented
+        elif self.method == "region_merging":
+            # Region Merging segmentation
+            method = self.kwargs.get('method', "slic")
+            if method == "slic":
+                segmented = slic(image, n_segments=100, compactness=10, sigma=1)
+            elif method == "quickshift":
+                segmented = quickshift(image, kernel_size=3, max_dist=6, ratio=0.5)
+            else:
+                raise ValueError("Invalid method specified")
+            return segmented
+        else:
+            raise ValueError("Invalid method specified")
