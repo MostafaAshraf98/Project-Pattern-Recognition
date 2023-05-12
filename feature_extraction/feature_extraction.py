74399694095466f81a5360fb23862f8c63656ab6
Nader-Youhanna <naderyouhanna@gmail.com>
2023-05-12 20:59:11 +0300

    Hog Features

--- feature_extraction/feature_extraction.py
+++ feature_extraction/feature_extraction.py
@@ -32,10 +32,22 @@ class FeatureExtractor:
     def __init__(self):
         pass
 
-    def extract_hog_features(self, images, hog_orientations=9,hog_pixels_per_cell=(8, 8), hog_cells_per_block=(2, 2)):
-        hog_features = hog(images, orientations=hog_orientations, pixels_per_cell=hog_pixels_per_cell, cells_per_block=hog_cells_per_block,
-                       visualize=False, multichannel=True)
-        return hog_features
+    def extract_hog_features(self, images, hog_orientations=9, hog_pixels_per_cell=(8, 8), hog_cells_per_block=(2, 2)):
+        if(len(images.shape) == 3):
+            hog_features = []
+            for i in range(images.shape[0]):
+                hog_features.append(hog(images[i], 
+                                orientations=hog_orientations,
+                                pixels_per_cell=hog_pixels_per_cell,
+                                cells_per_block=hog_cells_per_block,
+                                channel_axis = None))
+        else:
+            hog_features = hog( images, 
+                                orientations=hog_orientations,
+                                pixels_per_cell=hog_pixels_per_cell,
+                                cells_per_block=hog_cells_per_block,
+                                channel_axis = None)
+        return np.array(hog_features)
 
     def extract_lbp_features(self, images,lbp_num_points=8, lbp_radius=1):
         lbp_features = local_binary_pattern(images, lbp_num_points, lbp_radius)
@@ -43,15 +55,9 @@ class FeatureExtractor:
         return lbp_features
 
     def extract_sift_features(self, images, sift_num_features=128):
-        # sift = cv2.xfeatures2d.SIFT_create(128)
         sift = cv2.SIFT_create(sift_num_features)
         keypoints, sift_features = sift.detectAndComputeMulti(images, None)
         return sift_features
-
-    def extract_surf_features(self, images, surf_num_features=64):
-        surf = cv2.xfeatures2d.SURF_create(surf_num_features)
-        keypoints, surf_features = surf.detectAndComputeMulti(images, None)
-        return surf_features
 
     def extract_fourier_descriptor_features(self, images, num_coeffs=20):
         print(f'image shape: {images[0].shape}')
