29b6b81a85c9f394fa560cda4602d4204750f3dd
Mostafa <Mostafa.Ashraf@goldensetanalytics.com>
2023-05-12 21:43:41 +0300

    [FIX] fix the feature extraction and adding the feature extraction in main

--- feature_extraction/feature_extraction.py
+++ feature_extraction/feature_extraction.py
@@ -34,7 +34,7 @@ class FeatureExtractor:
 
     def extract_hog_features(self, images, hog_orientations=9,hog_pixels_per_cell=(8, 8), hog_cells_per_block=(2, 2)):
         hog_features = hog(images, orientations=hog_orientations, pixels_per_cell=hog_pixels_per_cell, cells_per_block=hog_cells_per_block,
-                       visualize=False, multichannel=True)
+                        block_norm='L2-Hys', visualize=False, feature_vector=True)
         return hog_features
 
     def extract_lbp_features(self, images,lbp_num_points=8, lbp_radius=1):
@@ -44,8 +44,15 @@ class FeatureExtractor:
 
     def extract_sift_features(self, images, sift_num_features=128):
         # sift = cv2.xfeatures2d.SIFT_create(128)
-        sift = cv2.SIFT_create(sift_num_features)
-        keypoints, sift_features = sift.detectAndComputeMulti(images, None)
+        sift = cv2.SIFT_create(nfeatures=sift_num_features)
+        keypoints = []
+        sift_features = []
+        for image in images:
+            k, s = sift.detectAndCompute(image, None)
+            keypoints.append(k)
+            sift_features.append(s)
+        keypoints = np.array(keypoints)
+        sift_features = np.array(sift_features)
         return sift_features
 
     def extract_surf_features(self, images, surf_num_features=64):
